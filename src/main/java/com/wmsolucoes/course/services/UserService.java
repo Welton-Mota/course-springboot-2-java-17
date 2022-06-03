e5fc6c5deba2568220548f5a9f93ca28e1c391e3
Welton Mota <welton.g.motta@gmail.com>
2022-06-03 16:18:37 -0300

    Service layer, component registration

---
+++ src/main/java/com/wmsolucoes/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.wmsolucoes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wmsolucoes.course.entities.User;
+import com.wmsolucoes.course.repositories.UserRepository;
+
+//anotation para registrar a minha classe como um componente do Spring
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
+
